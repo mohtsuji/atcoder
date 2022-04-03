a72d5b09141bc2c1f79e9f9b2ecfb83ac3b9a35e
mohtsuji <ohtsuji.mizuki@gmail.com>
2022-04-03 10:04:03 +0900

    finish 61

---
+++ atcoder/practice/tenkei90/55.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <numeric>
+#define ll long long
+
+using namespace std;
+
+int main()
+{
+    int n, p, q;
+    cin >> n >> p >> q;
+    vector<ll> a(n);
+    for (int i=0; i<n; i++) {
+        cin >> a[i];
+    }
+
+}
---
+++ atcoder/practice/tenkei90/61_deque.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main()
+{
+    int q;
+    cin >> q;
+    vector<int> t(q);
+    vector<int> x(q);
+    for (int i = 0; i < q; i++)
+        cin >> t[i] >> x[i];
+
+    deque<int> d;
+
+    for (int i = 0; i < q ; i++)
+    {
+        if (t[i] == 1)
+            d.push_front(x[i]);
+        else if (t[i] == 2)
+            d.push_back(x[i]);
+        else
+            cout << d[x[i] - 1] << "\n";
+    }
+    return 0;
+}
